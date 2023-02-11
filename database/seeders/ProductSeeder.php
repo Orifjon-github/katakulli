10c795e2b0f88ab3af527d393862de1c53221e71
Orifjon94 <avazbekoripov94@gmail.com>
2023-02-11 16:49:58 +0500

    Product Seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,9 +15,57 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $product = Product::create([
                 'category_id' => 1,
+                'name' => 'Venice Pizza',
+                'description' => 'description',
+                'image' => '/storage/products/product-1.png',
+                'price_small' => 5,
+                'price_medium' => 7,
+                'price_large' => 10,
+                'count' => 0,
+                'time' => 32,
+                'sub_title' => 'popular'
+            ]);
+            for ($b = 1; $b < 4; $b++) {
+                $product->sizes()->attach($b);
+            }
+        }for ($i = 0; $i < 5; $i++) {
+            $product = Product::create([
+                'category_id' => 2,
+                'name' => 'Venice Pizza',
+                'description' => 'description',
+                'image' => '/storage/products/product-1.png',
+                'price_small' => 5,
+                'price_medium' => 7,
+                'price_large' => 10,
+                'count' => 0,
+                'time' => 32,
+                'sub_title' => 'popular'
+            ]);
+            for ($b = 1; $b < 4; $b++) {
+                $product->sizes()->attach($b);
+            }
+        }for ($i = 0; $i < 5; $i++) {
+            $product = Product::create([
+                'category_id' => 3,
+                'name' => 'Venice Pizza',
+                'description' => 'description',
+                'image' => '/storage/products/product-1.png',
+                'price_small' => 5,
+                'price_medium' => 7,
+                'price_large' => 10,
+                'count' => 0,
+                'time' => 32,
+                'sub_title' => 'popular'
+            ]);
+            for ($b = 1; $b < 4; $b++) {
+                $product->sizes()->attach($b);
+            }
+        }for ($i = 0; $i < 5; $i++) {
+            $product = Product::create([
+                'category_id' => 4,
                 'name' => 'Venice Pizza',
                 'description' => 'description',
                 'image' => '/storage/products/product-1.png',
