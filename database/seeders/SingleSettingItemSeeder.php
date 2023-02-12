a8fed05303fa61fe5d324f989520403aeeea9c90
Orifjon94 <avazbekoripov94@gmail.com>
2023-02-13 00:18:10 +0500

    Partnership and Testimonials Seeder

--- database/seeders/SingleSettingItemSeeder.php
+++ database/seeders/SingleSettingItemSeeder.php
@@ -21,7 +21,9 @@ class SingleSettingItemSeeder extends Seeder
             'map' => 'https://www.google.com/maps/place/3+Charleville+Rd,+London+W14+9JL,+UK/@51.4891323,-0.2082222,17z/data=!3m1!4b1!4m6!3m5!1s0x48760f94050d9fa7:0x58e031d4d14b73ea!8m2!3d51.489129!4d-0.2060335!16s%2Fg%2F11c24k9034',
             'iframe_url' => 'https://youtu.be/sv3TXMSv6Lw',
             'image' => '/storage/homepage/setting/setting-image.png',
-            'partnership_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elitaa. Augue vivamus sit aliquam in velit volutpat scelerisque at id. Orci cursus egestas adipiscing quis id tempus donecc amet. Facilisi malesuada curabitur vulputate nulla tortor. vulputate adipiscing diam malesuada proin morbi mattiscc vel. Lorem ipsum dolor sit amet, consectetur adipiscing elitaa. Augue vivamus sit aliquam in velit volutpat scelerisque at id. Orci cursus egestas adipiscing quis id tempus donecc amet.  vivamus sit aliquam in velit volutpat scelerisque at id. Orci cursus egestas adipiscing quis id tempus donecc amet.  ',
+            'partnership_text' => 'Please fill out all the required fields in this form. Be as detailed as possible when describing your organization and what type of partnership you are seeking.
+After filling out all the required fields, click the "Submit" button to send your Partnership Request Form to our team for review.
+Disclaimer: Your submission of this form does not guarantee that a partnership will be established. Our team will review all submissions and reach out to you if there is a potential opportunity for partnership.',
         ]);
     }
 }
--- database/seeders/TestimonialSeeder.php
+++ database/seeders/TestimonialSeeder.php
@@ -16,20 +16,61 @@ class TestimonialSeeder extends Seeder
     public function run()
     {
         Testimonial::create([
-           'name' => 'Orifjon Oripov',
-           'body' => 'Juda ham zo\'r',
+           'name' => "Naushed Reza",
+           'body' => 'Our new favourite place to order pizza from! Can recommend enough!
+
+Very friendly staff, delivery is always on time, and most importantly, amazing pizzas. I would recommend  the Chicken Classic and the Vegetarian pizzas.
+
+Vegetarian options: Vegetarian Pizza',
             'rating' => 5,
             'image' => '/storage/homepage/users/user-1.png'
         ]);
         Testimonial::create([
-            'name' => 'Orifjon Oripov',
-            'body' => 'Juda ham zo\'r',
+            'name' => "Anca Pertu",
+            'body' => 'Friendly staff, fast service and amazing pizza, ordered once and kept coming back. Highly recommend! 💕',
             'rating' => 5,
             'image' => '/storage/homepage/users/user-1.png'
         ]);
         Testimonial::create([
-            'name' => 'Orifjon Oripov',
-            'body' => 'Juda ham zo\'r',
+            'name' => "Elenora Zeneli",
+            'body' => 'Amazing good pizzas for a good price as well! Very friendly too, def recommend!',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "Edvin",
+            'body' => 'Very nice Pizzeria, chef\'s works nice and clean, possible to get any pizza up to you as example I prefer tiny and crispy, so 5 Star ⭐️ ⭐️ ⭐️ ⭐️ ⭐️, recommend.',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "Andreina Ganata",
+            'body' => 'Very fast delivery
+Pizzas are very nice! Good quality, amazing taste',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "MR Randomizer",
+            'body' => '5 STAR DEFINITELY! It is a great place and the person who runs it, is very nice! I bought a pizza and it didn’t take very long. This is a pizza place with lots of fizzy drink/chocolate milk choices, ICE CREAM (as I’ve shown in the pictures) and sweets! Lots of sides and pizza. Do not judge this place by how small it is because the taste of the pizza is FANTASTIC.',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "Hamish Ryall",
+            'body' => 'Fantastic service. I ordered a pizza by mistake which had mushroom on it. I am allergic to mushroom. I called them and they sent me a different pizza free of charge. Could not be happier :))',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "Khakberdiev Akbar",
+            'body' => 'Such an amazing pizzas I have ever eat',
+            'rating' => 5,
+            'image' => '/storage/homepage/users/user-1.png'
+        ]);
+        Testimonial::create([
+            'name' => "Z Zaheer ",
+            'body' => 'Excellent. Top notch. Quality ingredients and delivered lovely and hot',
             'rating' => 5,
             'image' => '/storage/homepage/users/user-1.png'
         ]);
